49c3d05be6dd90a714951ed55f4d78002a095b18
Microsft-Wrd <mrjacobbrooks@gmail.com>
2026-02-10 13:22:03 +0000

    Working on maps

--- week4-maps/src/main.ts
+++ week4-maps/src/main.ts
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use("/leaflet", express.static(path.join(__dirname, "node_modules/leaflet/dist")))
 
 app.listen(5050, ()=> {
     console.log("Started Server")
